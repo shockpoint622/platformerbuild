e76246729e6a82e6151095371757ba333d9104c4
shockpoint <epyonascend622@gmail.com>
2026-07-26 14:45:48 -0400

    Problems, so many problems...

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class Game:
 
         self.display = pygame.Surface((320, 240))
 
-        self.movement = [False, False, False, False]
+        self.movement = [False, False]
 
         self.assets = {
             'decor': load_images('tiles/decor'),
@@ -29,16 +29,24 @@ class Game:
 
         self.tilemap = Tilemap(self, tile_size = 16)
 
+        self.scroll = [0,0]
+
     def run(self):
         def main():
             while True:
                 log_state()
                 self.display.fill((14,219,248))
 
-                self.tilemap.render(self.display)
+                self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30
+                self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
+                render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
 
-                self.player.update(((self.movement[3] - self.movement[2]),0))
-                self.player.render(self.display)
+                self.tilemap.render(self.display, offset = render_scroll)
+
+                self.player.update(self.tilemap, (self.movement[1] - self.movement[0],0))
+                self.player.render(self.display, offset = render_scroll)
+
+                #print(self.tilemap.physics_rects_around(self.player.pos))
 
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
@@ -46,22 +54,22 @@ class Game:
                         sys.exit()
                     if event.type == pygame.KEYDOWN:
                         if event.key == pygame.K_UP:
+                            self.player.velocity[1] = -3
+                        #if event.key == pygame.K_DOWN:
+                            #self.movement[1] = True
+                        if event.key == pygame.K_LEFT:
                             self.movement[0] = True
-                        if event.key == pygame.K_DOWN:
+                        if event.key == pygame.K_RIGHT:
                             self.movement[1] = True
-                        if event.key == pygame.K_LEFT:
-                            self.movement[2] = True
-                        if event.key == pygame.K_RIGHT:
-                            self.movement[3] = True
                     if event.type == pygame.KEYUP:
-                        if event.key == pygame.K_UP:
-                            self.movement[0] = False
-                        if event.key == pygame.K_DOWN:
-                            self.movement[1] = False
+                        #if event.key == pygame.K_UP:
+                            #self.movement[0] = False
+                        #if event.key == pygame.K_DOWN:
+                            #self.movement[1] = False
                         if event.key == pygame.K_LEFT:
-                            self.movement[2] = False
+                            self.movement[0] = False
                         if event.key == pygame.K_RIGHT:
-                            self.movement[3] = False
+                            self.movement[1] = False
 
 
 
