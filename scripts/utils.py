1d862737601c555573d02ac01e2d2e3f78f11b0b
shockpoint <epyonascend622@gmail.com>
2026-07-29 21:24:02 -0400

    Began working on the simplified animation system

--- scripts/utils.py
+++ scripts/utils.py
@@ -14,3 +14,24 @@ def load_images(path):
     for img_name in sorted(os.listdir(BASE_IMG_PATH + path)):
         images.append(load_image(path + '/' + img_name))
     return images
+
+class Animation:
+    def __init__(self, images, img_dur=5, loop=True):
+        self.images = images
+        self.img_dur = img_dur
+        self.loop = loop
+        self.done = False
+        self.frame = 0
+
+    def copy(self):
+        return Animation(self.images, self.img_dur, self.loop)
+
+    def update(self):
+        if self.loop:
+            self.frame = (self.frame + 1 ) % (self.img_dur * len(self.images))
+        else:
+            self.frame = min(self.frame + 1, self.img_dur * len(self.images) - 1)
+            if self.frame >= self.img_dur * len(self.images) - 1:
+                self.done = True
+    def img(self):
+        return self.images[int(self.frame / self.img_dur)]
