0201ae7e84c6758d431bc9d8034b5240d21590bb
shockpoint <epyonascend622@gmail.com>
2026-07-31 22:17:10 -0400

    Started on the Level Editor

---
+++ scripts/editor.py
@@ -0,0 +1,107 @@
+import pygame
+import sys
+from logger import log_state, log_event
+#from scripts.entities import *
+from scripts.utils import *
+from scripts.tilemap import *
+#from scripts.clouds import *
+
+RENDER_SCALE = 2.0
+
+class Editor:
+    def __init__(self):
+        pygame.init()
+
+        pygame.display.set_caption("editor")
+        self.clock = pygame.time.Clock()
+        #self.dt: float = 0.0
+        self.screen = pygame.display.set_mode((640,480))
+        self.display = pygame.Surface((320, 240))
+
+        self.assets = {
+            'decor': load_images('tiles/decor'),
+            'grass': load_images('tiles/grass'),
+            'large_decor': load_images('tiles/large_decor'),
+            'stone': load_images('tiles/stone'),
+        }
+
+        self.movement = [False, False, False, False]
+
+        self.tilemap = Tilemap(self, tile_size=16)
+
+        self.scroll = [0, 0]
+
+        self.tile_list = list(self.assets)
+        self.tile_group = 0
+        self.tile_variant = 0
+
+        self.clicking = False
+        self.right_clicking = False
+        self.shift = False
+
+    def run(self):
+
+        while True:
+            log_state()
+            self.display.fill((0,0,0))
+
+            current_tile_img = self.assets[self.tile_list[self.tile_group]][self.tile_variant].copy()
+            current_tile_img.set_alpha(100)
+
+            self.display.blit(current_tile_img, (5,5))
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        self.clicking = True
+                    if event.button == 3:
+                        self.right_clicking = True
+                    if self.shift:
+                        if event.button == 4:
+                            self.tile_variant = (self.tile_variant - 1) % len(self.tile_list)
+                        if event.button == 5:
+                            self.tile_variant = (self.tile_variant + 1) % len(self.tile_list)
+                    else:
+                        if event.button == 4:
+                            self.tile_group = (self.tile_group -1 ) % len(self.tile_list)
+                        if event.button == 5:
+                            self.tile_group = (self.tile_group +1) % len(self.tile_list)
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.movement[2] = True
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = True
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = True
+                    if even.key == pygame.K_DOWN:
+                        self.movement[3] = True
+                    if event.key == pygame.LSHIFT | pygame.RSHIFT:
+                        self.shift = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = False
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = False
+                    if event.key == pygame.K_UP:
+                        self.movement[2] = False
+                    if event.key == pygame.K_DOWN:
+                        self.movement[3] = False
+                    if event.key == pygame.LSHIFT | pygame.RSHIFT:
+                        self.shift = False
+
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0,0))
+            pygame.display.update()
+            self.clock.tick(60)
+
+
+
+
+    pygame.quit()
+
+
+Editor().run()
